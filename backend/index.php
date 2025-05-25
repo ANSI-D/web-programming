8897003f5bef9f760c21b9d5664f815bac6209fc
Danis <danis.pojskic@disroot.org>
2025-05-26 01:09:39 +0200

    Data tables added

--- backend/index.php
+++ backend/index.php
@@ -7,6 +7,7 @@ require_once 'rest/routes/category_routes.php';
 require_once 'rest/routes/comment_routes.php';
 require_once 'rest/routes/post_routes.php';
 require_once 'rest/routes/like_routes.php';
+require_once 'rest/routes/middleware_routes.php';
 require_once 'rest/routes/auth_routes.php';
 
 Flight::start();
