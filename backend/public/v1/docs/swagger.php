b3ad88f3227ad5b24acd760f9d194c78e6e0a113
Danis <danis.pojskic@disroot.org>
2025-06-08 23:30:49 +0200

    Edited variables

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../../../vendor/autoload.php';
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
    define('BASE_URL', 'http://localhost/danis-pojskic/web-programming/backend');
 } else {
-   define('BASE_URL', 'https://add-production-server-after-deployment/backend/');
+   define('BASE_URL', 'https://whattohost-57p29.ondigitalocean.app/backend/');
 }
 $openapi = \OpenApi\Generator::scan([
    __DIR__ . '/doc_setup.php',
